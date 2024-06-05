5e7d6906d1595921b2e3c999e69abf90030715da
jackavryashnik <jackavryashnik@gmail.com>
2024-06-05 14:26:27 +0200

    create routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'modern-normalize';
-import App from './App.jsx';
+import App from './components/App.jsx';
 import './index.css';
 import { Provider } from 'react-redux';
 // TODO:
