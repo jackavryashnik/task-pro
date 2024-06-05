6b2f3324765dac0b5a82d63579a766689dac75c8
jackavryashnik <jackavryashnik@gmail.com>
2024-06-05 16:13:00 +0200

    commented redux code that block program run

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
-import PrivateRout from './PrivateRoute/PrivateRoute';
-import RestrictedRoute from './RestrictedRoute/RestrictedRoute';
+// import PrivateRout from './PrivateRoute/PrivateRoute';
+// import RestrictedRoute from './RestrictedRoute/RestrictedRoute';
 
 const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
 const WelcomePage = lazy(() => import('../pages/WelcomePage/WelcomePage'));
@@ -15,31 +15,10 @@ function App() {
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/welcome" element={<WelcomePage />} />
-        <Route
-          path="/auth/login"
-          element={
-            <RestrictedRoute redirectTo="/contacts" component={<LoginForm />} />
-          }
-        />
-        <Route
-          path="/auth/register"
-          element={
-            <RestrictedRoute
-              redirectTo="/contacts"
-              component={<RegisterForm />}
-            />
-          }
-        />
-        <Route
-          path="/home"
-          element={<PrivateRout redirectTo="/login" component={<HomePage />} />}
-        >
-          <Route
-            path="/home/:boardID"
-            element={
-              <PrivateRout redirectTo="/login" component={<TaskBoard />} />
-            }
-          />
+        <Route path="/auth/login" element={<LoginForm />} />
+        <Route path="/auth/register" element={<RegisterForm />} />
+        <Route path="/home" element={<HomePage />}>
+          <Route path="/home/:boardID" element={<TaskBoard />} />
         </Route>
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
