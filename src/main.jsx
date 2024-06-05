799de5e259e52a4691cbba484113cdb1b42d24d0
jackavryashnik <jackavryashnik@gmail.com>
2024-06-05 11:49:13 +0200

    add base dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,20 @@ import ReactDOM from 'react-dom/client';
 import 'modern-normalize';
 import App from './App.jsx';
 import './index.css';
+import { Provider } from 'react-redux';
+// TODO:
+import { persistor, store } from './redux/store.js';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
