8cb5aca46df3657cb1d0ac211775c8dd131c9d6c
NIKOLAEVTOLIA <140529562+NIKOLAEVTOLIA@users.noreply.github.com>
2024-06-07 01:48:08 +0300

    upd 1  \ header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import css from './Header.module.css';
+
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [selectorOpen, setSelectorOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const handleThemeSelectorToggle = () => {
+    setSelectorOpen(!selectorOpen);
+  };
+
+  return (
+    <header className={css.header}>
+      <div className={css.menuBtnContainer}>
+        <button onClick={handleMenuToggle} className={css.menuButton}>
+          <svg className={css.menuIcon}>
+            <use href="#icon-burger-menu"></use>
+          </svg>
+        </button>
+      </div>
+      <div className={css.rightSection}>
+        <div className={css.themeSelectorContainer}>
+          <button
+            onClick={handleThemeSelectorToggle}
+            className={css.themeButton}
+          >
+            Theme
+            <svg className={css.chevronIcon}>
+              <use href="#icon-chevron-down"></use>
+            </svg>
+          </button>
+          {selectorOpen && (
+            <div className={css.themeSelector}>
+              <ul>
+                <li>Light</li>
+                <li>Violet</li>
+                <li>Dark</li>
+              </ul>
+            </div>
+          )}
+        </div>
+        <div className={css.userInfo}>
+          <span className={css.userName}>Ivetta</span>
+          <img
+            src="/src/images/bg-mobile/cloudy.webp"
+            alt="User Avatar"
+            className={css.avatar}
+          />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
