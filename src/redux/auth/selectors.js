93396355ab1c3ffcfe78b45cbfe0a679ff930b3c
Andreas-206 <puma_real@ukr.net>
2024-06-07 15:24:44 +0300

    +

---
+++ src/redux/auth/selectors.js
@@ -0,0 +1,8 @@
+export const selectUser = state => state.auth.user;
+export const selectToken = state => state.auth.token;
+export const selectUserName = state => state.auth.user.name;
+export const selectUserEmail = state => state.auth.user.email;
+export const selectIsLoggedIn = state => state.auth.isLoggedIn;
+export const selectIsRefreshing = state => state.auth.isRefreshing;
+export const selectIsLoading = state => state.auth.isLoading;
+export const selectUserTheme = state => state.auth.user.theme;
---
+++ src/redux/auth/slice.js
@@ -0,0 +1,92 @@
+import { createSlice } from '@reduxjs/toolkit';
+import {
+  register,
+  login,
+  logout,
+  updateUser,
+  getCurrentUser,
+  changeTheme,
+} from './operations.js'
+
+const handlePending = state => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
+const initialState = {
+  user: { 
+    name: null,
+    email: null,
+    theme: 'null',
+    avatarUrl: null,},
+  token: null,
+  isRefreshing: false,
+  isLoggedIn: false,
+  isLoading: false,
+  error: null,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  
+  extraReducers: builder => {
+    builder
+      .addCase(register.pending, handlePending)
+      .addCase(register.rejected, handleRejected)
+      .addCase(register.fulfilled, (state, action) => {
+        state.user.email = action.payload.email;
+        state.user.name = action.payload.name;
+        state.user.theme = action.payload.theme;
+        state.user.avatarUrl = action.payload.avatarUrl;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(login.pending, handlePending)
+      .addCase(login.rejected, handleRejected)
+      .addCase(login.fulfilled, (state, action) => {
+        state.user.email = action.payload.email;
+        state.user.name = action.payload.name;
+        state.user.theme = action.payload.theme;
+        state.user.avatarUrl = action.payload.avatarUrl;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(logout.fulfilled, state => {
+        state.user = { 
+          name: null,
+          email: null},
+        state.token = null;
+        state.isLoggedIn = false;
+  })
+      .addCase(getCurrentUser.pending, handlePending)
+      .addCase(getCurrentUser.rejected, handleRejected)
+      .addCase(getCurrentUser.fulfilled, (state, action) => {
+        state.user.email = action.payload.email;
+        state.user.name = action.payload.name;
+        state.user.theme = action.payload.theme;
+        state.user.avatarUrl = action.payload.avatarUrl;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+        state.isRefreshing = false;
+})  
+      .addCase(updateUser.fulfilled, (state, action) => {
+        state.user.name = action.payload.name;
+        state.user.avatarUrl = action.payload.avatarUrl;
+        state.isLoading = false;
+      })
+      .addCase(changeTheme.fulfilled, (state, action) => {
+        state.user.theme = action.payload.theme;
+      })
+  },
+});
+
+export const authReducer = authSlice.reducer;
