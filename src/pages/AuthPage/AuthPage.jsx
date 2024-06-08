a9795f11f1acc01f6f4d059ab2c755fa210d5343
ai_hikki <freeomareps@gmail.com>
2024-06-08 16:24:04 +0300

    Add LoginForm and RegistrationForm
    Add LoginForm and RegistrationForm markup

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -2,13 +2,18 @@ import { useParams } from 'react-router-dom';
 import LoginForm from '../../components/LoginForm/LoginForm';
 import RegisterForm from '../../components/RegisterForm/RegisterForm';
 import NotFoundPage from '../NotFoundPage/NotFoundPage';
+import css from './AuthPage.module.css';
 
 export default function AuthPage() {
-  const {authType} = useParams();
+  const { authType } = useParams();
 
   if (authType !== 'login' && authType !== 'register') {
-    return <NotFoundPage />
-  } 
+    return <NotFoundPage />;
+  }
 
-  return <div>{authType === 'login' ? <LoginForm /> : <RegisterForm />}</div>
+  return (
+    <div className={css.authPageContainer}>
+      {authType === 'login' ? <LoginForm /> : <RegisterForm />}
+    </div>
+  );
 }
