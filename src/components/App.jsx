1582bda76fb5a1873e425f537093ee7d1652e868
ai_hikki <freeomareps@gmail.com>
2024-06-11 21:03:05 +0300

    add theme handler

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
-import { lazy, Suspense } from 'react';
+import { lazy, Suspense, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import PrivateRout from './PrivateRoute/PrivateRoute';
 import RestrictedRoute from './RestrictedRoute/RestrictedRoute';
-import { selectIsRefreshing } from '../redux/auth/selectors';
+import { selectIsRefreshing, selectUserTheme } from '../redux/auth/selectors';
 
 const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
 const WelcomePage = lazy(() => import('../pages/WelcomePage/WelcomePage'));
@@ -11,8 +11,16 @@ const AuthPage = lazy(() => import('../pages/AuthPage/AuthPage'));
 const TaskBoard = lazy(() => import('./TaskBoard/TaskBoard'));
 const NotFoundPage = lazy(() => import('../pages/NotFoundPage/NotFoundPage'));
 
+const THEMES = ['light', 'dark', 'violet'];
+
 function App() {
   const isRefreshing = useSelector(selectIsRefreshing);
+  const theme = useSelector(selectUserTheme);
+
+  useEffect(() => {
+    document.body.classList.remove(...THEMES);
+    document.body.classList.add(theme);
+  }, [theme]);
 
   return (
     <>
@@ -31,16 +39,16 @@ function App() {
             <Route
               path="/home"
               element={
-                <PrivateRout
-                  redirectTo="/welcome"
-                  component={<HomePage />}
-                />
+                <PrivateRout redirectTo="/welcome" component={<HomePage />} />
               }
             >
               <Route
                 path="/home/:boardID"
                 element={
-                  <PrivateRout redirectTo="/welcome" component={<TaskBoard />} />
+                  <PrivateRout
+                    redirectTo="/welcome"
+                    component={<TaskBoard />}
+                  />
                 }
               />
             </Route>
