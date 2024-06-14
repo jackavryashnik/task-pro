54ca93ba35653d24f04af6ade490b87ac44e4da9
ValeriiaProkhina <applehouse1883@icloud.com>
2024-06-14 22:10:52 +0300

    column render

--- src/components/MainDashboard/MainDashboard.jsx
+++ src/components/MainDashboard/MainDashboard.jsx
@@ -5,30 +5,32 @@ import icons from '../../images/icons.svg';
 import AddColumnModal from '../AddColumnModal/AddColumnModal';
 
 const MainDashboard = ({ openModal, closeModal }) => {
-  const { columns = [] } = useTasks();
+  const { columns = [], boards } = useTasks();
 
   return (
     <div className={css.dashboard}>
-      <div className={css.columns}>
-        {columns.map(column => (
-          <Column
-            openModal={openModal}
-            closeModal={closeModal}
-            key={column.id}
-            column={column}
-            tasks={column.tasks}
-          />
-        ))}
-        <button
-          onClick={() => openModal(<AddColumnModal onClose={closeModal} />)}
-          className={css.addColumnButton}
-        >
-          <svg className={css.addIcon} width={28} height={28}>
-            <use href={`${icons}#icon-plus`}></use>
-          </svg>
-          Add another column
-        </button>
-      </div>
+      {boards.length > 0 && (
+        <div className={css.columns}>
+          {columns.map(column => (
+            <Column
+              openModal={openModal}
+              closeModal={closeModal}
+              key={column.id}
+              column={column}
+              tasks={column.tasks}
+            />
+          ))}
+          <button
+            onClick={() => openModal(<AddColumnModal onClose={closeModal} />)}
+            className={css.addColumnButton}
+          >
+            <svg className={css.addIcon} width={28} height={28}>
+              <use href={`${icons}#icon-plus`}></use>
+            </svg>
+            Add another column
+          </button>
+        </div>
+      )}
     </div>
   );
 };
