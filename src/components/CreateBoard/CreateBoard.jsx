3be97485d48cd93130fd925f4382973d7011658e
ValeriiaProkhina <applehouse1883@icloud.com>
2024-06-16 21:12:22 +0300

    fix isEdit

--- src/components/CreateBoard/CreateBoard.jsx
+++ src/components/CreateBoard/CreateBoard.jsx
@@ -65,7 +65,7 @@ export default function CreateBoard({ onClose, isEdit, setIsEdit }) {
         background: selectedBackground.name,
       })
     );
-
+    setIsEdit(true);
     onClose();
   };
 
