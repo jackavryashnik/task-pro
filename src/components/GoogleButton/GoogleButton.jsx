0bf172899607b25594077966436f59c49cbf8fa7
Antony Ishchenko <antony.ishchenko@gmail.com>
2024-06-17 23:57:28 +0300

    google-auth

--- src/components/GoogleButton/GoogleButton.jsx
+++ src/components/GoogleButton/GoogleButton.jsx
@@ -1,16 +1,9 @@
 import css from './GoogleButton.module.css';
 import icon from '../../images/icons.svg';
 
-export const GoogleButton = ({ text, onClick }) => {
+export const GoogleButton = ({ text }) => {
   return (
-    <a
-      className={css.button}
-      role="button"
-      onClick={e => {
-        e.preventDefault();
-        onClick();
-      }}
-    >
+    <a className={css.button} href="http://localhost:8000/api/auth/google">
       <svg width={40} height={40} className={css.googleImg}>
         <use href={`${icon}#icon-google`}></use>
       </svg>
