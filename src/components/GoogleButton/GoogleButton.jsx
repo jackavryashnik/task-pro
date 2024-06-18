2a900945a9397a23f86aef832375bafd4ef7818a
Antony Ishchenko <antony.ishchenko@gmail.com>
2024-06-18 09:30:47 +0300

    fix-url

--- src/components/GoogleButton/GoogleButton.jsx
+++ src/components/GoogleButton/GoogleButton.jsx
@@ -3,7 +3,10 @@ import icon from '../../images/icons.svg';
 
 export const GoogleButton = ({ text }) => {
   return (
-    <a className={css.button} href="http://localhost:8000/api/auth/google">
+    <a
+      className={css.button}
+      href="https://task-pro-app-0x3n.onrender.com/api/auth/google"
+    >
       <svg width={40} height={40} className={css.googleImg}>
         <use href={`${icon}#icon-google`}></use>
       </svg>
